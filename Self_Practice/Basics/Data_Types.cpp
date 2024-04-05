43bd11ec7796a7137dcdd0938d0aef5f24f65d69
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-05 20:51:58 +0530

    explored data types in cpp

---
+++ Self_Practice/Basics/Data_Types.cpp
@@ -0,0 +1,18 @@
+// Implementation of data types in C++.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    float b = 10.5;
+    char c = 'A';
+    bool d = true;
+    double e = 10.5;
+    cout << "Size of int: " << sizeof(a) << endl;
+    cout << "Size of float: " << sizeof(b) << endl;
+    cout << "Size of char: " << sizeof(c) << endl;
+    cout << "Size of bool: " << sizeof(d) << endl;
+    cout << "Size of double: " << sizeof(e) << endl;
+    return 0;
+}
