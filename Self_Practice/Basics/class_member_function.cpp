a01a8dc529d1c16b701c80c19cd23a56ac34556c
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-06 14:20:27 +0530

    Update class_member_function.cpp

--- Self_Practice/Basics/class_member_function.cpp
+++ Self_Practice/Basics/class_member_function.cpp
@@ -21,3 +21,32 @@ int main() {
     return 0;
 }
 // Output: Hello, my name is John and I am 25 years old
+
+// making the class member function more complex.
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+class Human {
+    public:
+        string name;
+        int age;
+        void introduce() {
+            cout << "Hello, my name is " << name << " and I am " << age << " years old" << endl;
+        }
+        void say_hello() {
+            cout << "Hello, World!" << endl;
+        }
+};
+
+int main() {
+    Human human1;
+    human1.name = "John";
+    human1.age = 25;
+    human1.introduce();
+    human1.say_hello();
+    return 0;
+}
+
+// Output: Hello, my name is John and I am 25 years old
