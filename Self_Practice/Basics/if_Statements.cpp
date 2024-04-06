16af77d7da7cb6f0bf1014cb8e57df1f20d4567e
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-06 06:40:13 +0530

    setup the vs code little bit

---
+++ Self_Practice/Basics/if_Statements.cpp
@@ -0,0 +1,17 @@
+// implementing if statements  in C++
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    if (age >= 18) {
+        cout << " You are eligible to vote"<<endl;
+        }
+    else {
+        cout << " You are not eligible to vote"<<endl;
+        } 
+        return 0;
+    }
+    
+    
