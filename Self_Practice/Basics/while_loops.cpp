29f8608278188be7a115c6820bdda1a7d8d2aa0d
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-06 07:06:21 +0530

    while loops

---
+++ Self_Practice/Basics/while_loops.cpp
@@ -0,0 +1,13 @@
+// implementing while loops in C++
+// //
+#include <iostream>
+using namespace std;
+
+int main () {
+    int i = 1;
+    while (i <= 5) {
+        cout << i << endl;
+        i++;
+    }
+    cout << "Loop ends here" << endl;
+}
