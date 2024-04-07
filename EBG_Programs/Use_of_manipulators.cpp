caeb292f22c133459cdd59cdf6839b06c89a2023
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-07 22:34:37 +0530

    Manipulators

---
+++ EBG_Programs/Use_of_manipulators.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Administrator on 07-Apr-24.
+//
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    int basic = 1000, da = 200, hra = 300;
+    cout<< setw(10) << "Basic" << setw(10) << "DA" << setw(10) << "HRA" << setw(10) << "Total" << endl;
+}
