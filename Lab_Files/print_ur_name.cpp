1e36a6f29f322b291db1b24466d0e4208cccefd1
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-04-07 22:39:37 +0530

    lab1_1

---
+++ Lab_Files/print_ur_name.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 07-Apr-24.
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+
+    cout << "Enter the name: ";
+    getline(cin,name);
+    cout << "Entered name is: " << name;
+    return 0;
+}
