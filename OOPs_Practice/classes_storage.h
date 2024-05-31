a3fe23ec77409ddd662a594d29b80b8d2e195cd2
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-05-31 10:38:24 +0530

    Create classes_storage.h

---
+++ OOPs_Practice/classes_storage.h
@@ -0,0 +1,22 @@
+//
+// Created by Administrator on 31-May-24.
+//
+
+#ifndef OOPS_CSVTU_SEM02_CLASSES_STORAGE_H
+#define OOPS_CSVTU_SEM02_CLASSES_STORAGE_H
+
+
+class Hero {
+    char name[20];
+    int health;
+    int level;
+
+};
+
+class Enemy {
+    char name[20];
+    int health;
+    int level;
+};
+
+#endif //OOPS_CSVTU_SEM02_CLASSES_STORAGE_H
