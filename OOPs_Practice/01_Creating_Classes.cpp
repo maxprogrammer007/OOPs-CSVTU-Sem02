ac22c52f8e873f7bd147251225ca92135e1fbab7
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-05-31 11:24:44 +0530

    Update 01_Creating_Classes.cpp

--- OOPs_Practice/01_Creating_Classes.cpp
+++ OOPs_Practice/01_Creating_Classes.cpp
@@ -8,4 +8,12 @@ int main() {
     cout << sizeof(x1) << endl;
     Enemy y1;
     cout << sizeof(y1) << endl;
+
+    x1.health = 100;
+    x1.setName("Hero");
+    x1.setLevel(5);
+    x1.printName();
+    x1.printHealth();
+    cout << x1.getLevel() << endl;
+    return 0;
 }
