3ac7603b52300ea8a9e61026ea41b4e7ed9854d6
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-05-31 10:38:29 +0530

    Update 01_Creating_Classes.cpp

--- OOPs_Practice/01_Creating_Classes.cpp
+++ OOPs_Practice/01_Creating_Classes.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
+#include "classes_storage.h"
 using namespace std;
 
-class Hero{
-    char name[20];
-    int health;
-    int level;
-};
 
 int main() {
     Hero x1;
     cout << sizeof(x1) << endl;
+    Enemy y1;
+    cout << sizeof(y1) << endl;
 }
