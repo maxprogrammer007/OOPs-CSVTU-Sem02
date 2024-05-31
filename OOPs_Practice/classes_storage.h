2e7ac0a577fb039f734ba249c4176b2c9f955173
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-05-31 11:24:47 +0530

    Update classes_storage.h

--- OOPs_Practice/classes_storage.h
+++ OOPs_Practice/classes_storage.h
@@ -5,10 +5,29 @@
 #ifndef OOPS_CSVTU_SEM02_CLASSES_STORAGE_H
 #define OOPS_CSVTU_SEM02_CLASSES_STORAGE_H
 
+#include<cstring>
+#include<iostream>
 
 class Hero {
+    public:
     char name[20];
     int health;
+    void setName(char n[]) {
+        strcpy(name, n);
+    }
+    void printName() {
+        std::cout << name << std::endl;
+    }
+    void printHealth() {
+        std::cout << health << std::endl;
+    }
+    void setLevel(int l) {
+        level = l;
+    }
+    int getLevel() {
+        return level;
+    }
+    private:
     int level;
 
 };
